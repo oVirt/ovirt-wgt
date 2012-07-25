0aabd8f6371bbee1cff5de1f4ad8e4dec7522793
Christophe Fergeau <cfergeau@redhat.com>
2012-07-25 09:51:21 +0200

    boxes: add script to generate boxes unattended driver ISO
    The ISO will contain the viostor drivers so that the automatic
    install can be done on a virtio disk.

---
+++ tools/gen-boxes-unattended-win-iso.py
@@ -0,0 +1,22 @@
+#/usr/bin/python
+
+import iso
+import os
+import shutil
+import virtio
+
+tempdir = "tmp"
+os.makedirs(tempdir)
+mountpoint = os.path.join(tempdir, "mnt")
+base_dest = os.path.join(tempdir, "boxes")
+drivers_dest = os.path.join(base_dest, "preinst")
+output_iso_name = "boxes-unattended-win-drivers.iso"
+
+iso_name = virtio.download_iso(tempdir)
+iso_path = os.path.join(tempdir, iso_name)
+with iso.IsoMounter(iso_path, mountpoint):
+        virtio.copy_drivers(mountpoint, drivers_dest)
+shutil.copy(os.path.join("boxes-unattended-win-drivers", "README"), base_dest)
+shutil.copy(os.path.join("boxes-unattended-win-drivers", "txtsetup.oem"), os.path.join(drivers_dest, "winxp", "x86"))
+iso.geniso(base_dest, output_iso_name)
+print "done"
