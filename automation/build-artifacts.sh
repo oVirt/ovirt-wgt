b0e3e40cce2aaa11ea339d193cb7626431c8a670
Yedidyah Bar David <didi@redhat.com>
2017-12-06 09:54:41 +0200

    packaging: Handle release suffix
    Make artifacts optionally have TIMESTAMP.gitHASH as release suffix.
    
    Change-Id: Ie6f0680ec0e9d02ea660ed150fcfb5f9317a149e
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -6,10 +6,16 @@
 || mkdir -p tmp.repos/SOURCES
 
 make dist
+
+SUFFIX=
+. automation/config.sh
+[ -n "${RELEASE_SUFFIX}" ] && SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 yum-builddep -y ovirt-guest-tools-iso.spec
 
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    ${SUFFIX:+-D "release_suffix ${SUFFIX}"} \
     -ta *.tar.bz2
 
 mv *.tar.bz2 exported-artifacts
